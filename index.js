5d6eb9ea9168af23743db87d8ad7fd4db5afe2ee
Daisy Simiyu <d.daisysimiyu@gmail.com>
2022-06-10 16:40:43 +0300

    commit tree ec650cb6bc6c2105eaf54ef8941ef6ffa54098c3

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-
 // Write your code here
+let num1=31;
+let num2=2;
+const multiply = num1*num2;
+
+const random = Math.floor(Math.random() * 10) +1
+
+
+
+const num3 = 40;
+const num4 = 6;
+
+const mod = num3/num4
+
+const max = Math.max(1,4,5,20,19,8,6,3)
